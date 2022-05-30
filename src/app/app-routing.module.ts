4556f149a6c56d14fe37bf15bd9f14160d7b8514
Tino Garcia <tino@csengine.net>
2022-05-31 01:35:06 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EquiposComponent } from './equipos/equipos.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { JugadoresComponent } from './jugadores/jugadores.component';
+import { JugadorComponent } from './jugador/jugador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'equipos', component: EquiposComponent},
+  { path: 'equipo/:idTeam', component: EquipoComponent} ,
+  { path: 'jugadores', component: JugadoresComponent} ,
+  { path: 'jugador/:idJugador', component: JugadorComponent} ,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EquiposComponent } from './equipos/equipos.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { JugadoresComponent } from './jugadores/jugadores.component';
+import { JugadorComponent } from './jugador/jugador.component';
+
+import { HttpClientModule } from "@angular/common/http";
+
+import { ApiService } from './servicio/api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EquiposComponent,
+    EquipoComponent,
+    JugadoresComponent,
+    JugadorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
